d3f2aaec4bc95efd223bbe878271aa64e89bf149
Aaron Roberson <Aaron.Roberson@nike.com>
2016-03-17 14:22:05 -0700

    feat(sourceType): add sourceType option

--- examples/jquery/files/src_css_var_isHidden_js/report.history.js
+++ examples/jquery/files/src_css_var_isHidden_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"2016-01-17T20:04:06.000Z","sloc":16,"lloc":4,"functions":2,"deliveredBugs":0.05563602068853097,"maintainability":84.42353664346192,"lintErrors":[],"difficulty":8.25}]
+__history = [{"date":"2016-01-17T22:18:53.000Z","sloc":16,"lloc":4,"functions":2,"deliveredBugs":0.05563602068853097,"maintainability":84.42353664346192,"lintErrors":[],"difficulty":8.25}]
